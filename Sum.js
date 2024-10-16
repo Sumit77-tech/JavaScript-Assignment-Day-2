c84773fe05bd9bfe1cab3f20c00aade3518d99ae
SUMIT gourav <sumit.gourav280800@gmail.com>
2024-10-16 14:18:01 -0500

    Second commit

---
+++ Sum.js
@@ -0,0 +1,11 @@
+let N = 3
+let M = 2
+let arr = [[1, 2], [3, 4], [5, 6]]
+
+let sum = 0
+for(let i = 0; i < N; i++) {
+    for(let j = 0; j < M; j++) {
+        let sum = i + j
+        console.log(sum)
+    }
+}
